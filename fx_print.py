bc28723a654d0bfcc9c38cadd3684810ee349bc2
Andrew Fitzgibbon <awf@graphcore.ai>
2022-12-28 17:29:47 +0000

    use torch.compile rather than symbolic_trace

--- fx_print.py
+++ fx_print.py
@@ -1,3 +1,4 @@
+import re
 import torch
 
 
@@ -8,31 +9,46 @@ def _commajoin(vs):
 def fn_name(f):
     n = f.__name__
     if hasattr(f, "__module__") and f.__module__ != "_operator":
-        n = f"{f.__module__}.{n}"
+        if f.__module__ == 'torch._ops.aten':
+          n = f"aten.{n}"
+        else:
+          n = f"{f.__module__}.{n}"
 
     if hasattr(f, "__code__"):
         n += f"[{f.__code__.co_filename}:{f.__code__.co_firstlineno}]"
 
     return n
 
+_default_ignore = {"creation_timestamp", "stack_trace"}
 
-def fx_print_node(
-    node, gm=None, name2ord=None, ignore={"creation_timestamp", "stack_trace"}
-):
+def fx_print_node(node, gm=None, name2ord=None, ignore=_default_ignore):
     def argstr(a):
         if isinstance(a, tuple):
             return "(" + _commajoin(map(argstr, a)) + ")"
+        if isinstance(a, (list, torch.fx.immutable_collections.immutable_list)):
+            return "[" + _commajoin(map(argstr, a)) + "]"
         if isinstance(a, torch.fx.Node):
             return name2ord[a.name] if name2ord else a.name
-        if isinstance(a, (int, float)):
+        if isinstance(a, (float,)):
             return f"{type(a).__name__}({a})"
+        if isinstance(a, int):
+            return f"{a}"
         if isinstance(a, str):
             return f"'{a}'"
 
-        return str(a) + f"[{type(a)}]"
+        return f"{type(a)}{repr(a)}"
 
     def prshape(s: torch.Size):
-      return 'x'.join(str(s) for s in s)
+        return "x".join(str(s) for s in s)
+
+    def value_str(v):
+        vstr = str(v)
+        vstr = re.sub(r"\s*\n\s*", r"\\n ", vstr)
+        if len(vstr) > 40:
+            vstr = vstr[:30] + "<...>" + vstr[-10:]
+        if isinstance(v, torch.Tensor):
+            vstr = f"Tensor[{prshape(v.shape)}, {v.dtype}]({vstr})"
+        return vstr
 
     print_meta_handlers = {
         # tensor_meta:TensorMetadata(shape=torch.Size([2, 32]), dtype=torch.float32, requires_grad=False, stride=(32, 1), memory_format=torch.contiguous_format, is_quantized=False, qparams={})
@@ -52,7 +68,7 @@ def fx_print_node(
             if k not in ignore and k not in print_meta_handlers
         ]
     )
-    comment = f" # {meta_strs}"
+    comment = f" # {meta_strs}" if len(meta_strs) > 0 else ''
 
     if node.op == "output":
         return f"return {argstrs[0]}{comment}"
@@ -75,33 +91,40 @@ def fx_print_node(
         if gm:
             if hasattr(gm, node.target):
                 val = getattr(gm, node.target)
-                valstr = str(val).replace("\n", "\\n")[:40]
+                valstr = value_str(val)
             else:
                 valstr = f"no attr {node.target}"
-        else:
-            valstr = "[no GM]"
-        return f"{lhs} = getattr({node.target}, {node.args}) # {valstr}"
+
+            assert node.args == ()
+            return f"{lhs} = self.{node.target} # {valstr}"
+
+        return f"{lhs} = getattr({node.target}, {node.args})"
 
     return (
         f"# unhandled {node.op} {lhs} = {node.target}({_commajoin(argstrs)}){comment}"
     )
 
 
-def fx_print(gm):
+def fx_print_iter(gm, ignore=_default_ignore):
 
     name2ord = {}
     ord = 10
 
     args = [n.name for n in gm.graph.nodes if n.op == "placeholder"]
     name = torch.nn.Module._get_name(gm)
-    print(f"def {name}({_commajoin(args)}):")
+    yield f"def {name}({_commajoin(args)}):"
+
     for node in gm.graph.nodes:
         assert node.name not in name2ord
         name2ord[node.name] = f"v{ord}"
         ord += 1
 
-        node_str = fx_print_node(node, gm, name2ord)
-        print("  " + node_str)
+        yield "  " + fx_print_node(node, gm, name2ord, ignore)
+
+def fx_print(gm, ignore=_default_ignore):
+
+    for line in fx_print_iter(gm, ignore):
+        print(line)
 
 
 def test_fx_print():
@@ -112,15 +135,31 @@ def test_fx_print():
 
     def my_func(x, b):
         y = 2 * x
-        for _ in range(2):  # Loops will be unrolled
+        for _ in range(b):  # Loops will be unrolled
             x = aux(x, y)  # Function calls will be inlined
         x = x.neg()
         x = x @ torch.eye(3)
-        return torch.atan2(b, x)
+        x = torch.transpose(x, 1, 0)
+        x = torch.sum(x, [0, 1])
 
-    gm = torch.fx.symbolic_trace(my_func)
+        return torch.atan2(torch.tensor(float(b)), x)
 
-    fx_print(gm)
+    my_func(torch.rand(3, 3), 5)
+
+    def compiler_fn(fx_module: torch.fx.GraphModule, example_inputs):
+        fx_print(fx_module)
+        return fx_module
+
+    aux_gm = torch.compile(aux, backend=compiler_fn)
+
+    aux_gm(torch.rand(3, 3, device='meta'), torch.rand(3,3, device='meta'))
+
+    # Pass on the compiler_fn to the aot_function API
+    my_func_gm = torch.compile(my_func, backend=compiler_fn)
+
+    my_func_gm(torch.rand(3, 3), 5)
+
+    print("done")
 
 
 if __name__ == "__main__":
