3595ba8a7789cb5cc80db6d23f2af52745507827
Andrew Fitzgibbon <awf@graphcore.ai>
2023-01-17 14:09:07 +0000

    move tmps to tmp

--- dynamo_mnist.py
+++ dynamo_mnist.py
@@ -32,7 +32,7 @@ def compiler_savegraph(gm: torch.fx.GraphModule, example_inputs: List[torch.Tens
     counts[name]+=1
     name = name + '_' + str(counts[name])
 
-    filename = f'tmp_{name}.py'
+    filename = f'tmp/{name}.py'
     fx_print_to_file(gm, filename)
 
     return gm
